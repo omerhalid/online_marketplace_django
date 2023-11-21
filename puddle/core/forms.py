ed2460bc3af841e7286d4669e63350ae3e04bae0
omerhalid <omerhalidcinar@gmail.com>
2023-11-21 14:57:07 +0100

    Add form styling and error handling to signup page

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -7,3 +7,8 @@ class SignupForm(UserCreationForm): # This is a form that inherits from the User
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Username'})) # This is a field that inherits from the CharField. It has a widget that inherits from the TextInput. It has a class attribute that is a string. It has a placeholder attribute that is a string.
+    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Email'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Password'}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Repeat Password'}))

3a2e9641a3752762e4c29d5fe6468b7f5cff394d
omerhalid <omerhalidcinar@gmail.com>
2023-11-21 22:34:51 +0100

    Update login functionality

--- puddle/core/forms.py
+++ puddle/core/forms.py
@@ -1,7 +1,16 @@
 from django import forms
 
 from django.contrib.auth.forms import UserCreationForm # This is a built-in form from Django.
+from django.contrib.auth.forms import AuthenticationForm # This is a built-in form from Django.
 from django.contrib.auth.models import User # This is a built-in model from Django.
+
+class LoginFrom(AuthenticationForm): # This is a form that inherits from the AuthenticationForm.
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+        
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Username'})) # This is a field that inherits from the CharField. It has a widget that inherits from the TextInput. It has a class attribute that is a string. It has a placeholder attribute that is a string.
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Password'}))
 
 class SignupForm(UserCreationForm): # This is a form that inherits from the UserCreationForm.
     class Meta:
@@ -12,3 +21,4 @@ class SignupForm(UserCreationForm): # This is a form that inherits from the User
     email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Email'}))
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Password'}))
     password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'w-full py-4 px-6 rounded-xl', 'placeholder': 'Repeat Password'}))
+    
--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
+from .forms import LoginFrom
 
 app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
-    path('signup/', views.singup, name='signup')
+    path('signup/', views.singup, name='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name='core/login.html' ,authentication_form=LoginFrom), name='login'),
 ]

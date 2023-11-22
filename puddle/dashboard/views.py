db3c7d514d98b6722d14cc9fac1b52af243dafee
omerhalid <omerhalidcinar@gmail.com>
2023-11-22 11:26:22 +0100

    Fix dashboard views and add delete functionality

--- puddle/dashboard/views.py
+++ puddle/dashboard/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from item.models import Item
 
 @login_required
 def index(request):
-    items = Item.objects.filter(created_by=request.user)
+    items = Item.objects.filter(created_by=request.user) # get all items created by the user
     
     return render(request, 'dashboard/index.html', {'items': items})
